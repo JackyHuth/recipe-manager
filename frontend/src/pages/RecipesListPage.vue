03485293a83de295f045b253c20e316da7475930
Jaqueline Huth <137807502+JackyHuth@users.noreply.github.com>
2025-06-27 00:24:29 +0200

    Created recipe page files

---
+++ frontend/src/pages/RecipesListPage.vue
@@ -0,0 +1,9 @@
+<template>
+    <q-page class="q-pa-md">
+      <h1>My Recipes</h1>
+      <!-- Recipe cards, search, filters, etc. -->
+    </q-page>
+</template>
+
+<script setup lang="ts">
+</script>
